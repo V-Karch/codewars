d87ae0793db3171b0b2c122cd460d25e2637bdad
V-Karch <kluna0905@gmail.com>
2024-08-26 12:49:25 -0400

    DetectPangram: Fixed accidental '#' delection

--- DetectPangram/solution.rb
+++ DetectPangram/solution.rb
@@ -3,6 +3,6 @@ require 'set'
 def pangram?(string)
     alphabet = Set.new('a'..'z') # Make a set for comparison
     characters = string.downcase.chars.select { |char| char.match?(/[a-z]/) }.to_set
-    ^^ Convert all characters to lowercase and match only alphabetic characters as a set
+    # ^^ Convert all characters to lowercase and match only alphabetic characters as a set
     return characters >= alphabet # Compare the two sets and return the result
 end
